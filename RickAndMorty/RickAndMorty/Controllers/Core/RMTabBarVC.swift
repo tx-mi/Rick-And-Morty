cf37b4e7ed2752d7e92d33918350c9b981829489
Ramazan <txmiwxxd@gmail.com>
2023-01-05 21:00:03 +0300

    create empty project

---
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMTabBarVC.swift
@@ -0,0 +1,17 @@
+//
+//  RMTabBarVC.swift
+//  RickAndMorty
+//
+//  Created by Ramazan Abdulaev on 05.01.2023.
+//
+
+import UIKit
+
+final class RMTabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
