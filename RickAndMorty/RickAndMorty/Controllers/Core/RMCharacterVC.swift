4e6fa226fa7f6c922fc16609d33c8fb31831bc4c
Ramazan <txmiwxxd@gmail.com>
2023-01-06 02:09:51 +0300

    started character list view - now like a placeholder; add ext for uiview

--- RickAndMorty/RickAndMorty/Controllers/Core/RMCharacterVC.swift
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMCharacterVC.swift
@@ -10,21 +10,28 @@ import UIKit
 /// Controller to show and search for characters
 class RMCharacterVC: UIViewController {
     
+    // MARK: - Private properties
+    private let characterListView = CharacterListView()
+    
+    // MARK: - Public properties
+    // MARK: - Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Characters"
         view.backgroundColor = .systemBackground
         
-        RMService.shared.execute(.listCharactersRequest,
-                                 expecting: RMGetAllCharacterResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print(String(describing: model))
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
-        
+        view.addSubview(characterListView)
+        makeConstraints()
+    }
+    
+    // MARK: - Private methods
+    private func makeConstraints() {
+        NSLayoutConstraint.activate([
+            characterListView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            characterListView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
 }
