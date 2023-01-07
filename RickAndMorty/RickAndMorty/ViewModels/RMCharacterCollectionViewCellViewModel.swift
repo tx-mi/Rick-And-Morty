efcd33b8dd4c43e9a463651fe67d0694fabfcd8d
Ramazan <txmiwxxd@gmail.com>
2023-01-07 03:38:02 +0300

    imageloader(caching images)

--- RickAndMorty/RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -39,14 +39,6 @@ final class RMCharacterCollectionViewCellViewModel {
             completion(.failure(URLError(.badURL)))
             return
         }
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data, error == nil else {
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-            completion(.success(data))
-        }
-        task.resume()
+        RMImageLoader.shared.downloadImage(url: url, completion: completion)
     }
 }
